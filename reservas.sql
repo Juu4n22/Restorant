d904d8d19522af2f6c45c5aa309866ed6fc7609f
Juan Pablo Paredes <35340749@ifts24.edu.ar>
2024-11-05 00:59:19 -0300

    base de datos sql
    ultima copia de la base de datos

--- reservas.sql
+++ reservas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-11-2024 a las 18:46:11
+-- Tiempo de generación: 05-11-2024 a las 04:58:26
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -104,8 +104,10 @@ CREATE TABLE `reservas` (
 --
 
 INSERT INTO `reservas` (`id_reserva`, `nombre_cliente`, `fecha_hora`, `num_personas`, `created_at`, `updated_at`, `id_mesa`, `id_turno`) VALUES
-(11, 'Carlos Rodríguez', '2024-11-15 19:30:00', 4, '2024-10-17 00:17:23', '2024-10-17 00:17:23', 2, 2),
-(13, 'Familia Gómez', '2024-11-17 13:15:00', 6, '2024-10-17 00:17:23', '2024-10-17 00:17:23', 8, 1);
+(11, 'Carlos Rodríguez', '2024-11-15 22:30:00', 4, '2024-10-17 00:17:23', '2024-11-03 23:42:10', 2, 2),
+(13, 'Familia Gómez', '2024-11-17 13:15:00', 6, '2024-10-17 00:17:23', '2024-10-17 00:17:23', 8, 1),
+(14, 'juampiiii', '2024-11-16 01:30:00', 3, '2024-11-03 16:15:44', '2024-11-03 16:18:51', 3, 1),
+(15, 'TOMAS VINOLI', '2024-11-16 01:30:00', 9, '2024-11-03 16:16:58', '2024-11-05 03:40:37', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -127,7 +129,7 @@ CREATE TABLE `turnos` (
 --
 
 INSERT INTO `turnos` (`id_turno`, `hora_inicio`, `hora_fin`, `descripcion`, `created_at`, `updated_at`) VALUES
-(1, '12:00:00', '16:00:00', 'Turno de almuerzo', '2024-10-17 00:17:19', '2024-10-17 00:17:19'),
+(1, '12:00:00', '16:00:00', 'Turno de almuerzo', '2024-10-17 00:17:19', '2024-11-05 03:31:20'),
 (2, '18:00:00', '22:00:00', 'Turno de cena', '2024-10-17 00:17:19', '2024-10-17 00:17:19');
 
 -- --------------------------------------------------------
@@ -152,7 +154,13 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre`, `email`, `password`, `rol`, `imagen`, `created_at`, `updated_at`) VALUES
-(4, 'Admin User', 'admin@example.com', '$2y$10$jxaibnwMLW0Gp3heqWx6iO19eWiM3I.ctP74FeNgmGw2KenCOIiqK', 'admin', NULL, '2024-11-02 16:41:41', '2024-11-02 16:41:41');
+(8, 'Nuevo Administrador', 'admin@ejemplo.com', '$2b$10$dFUNUhzfmWqFizMmnweZXuU4DP6OD6O1JZf.IwkzGzQptnd0QI2I.', 'admin', NULL, '2024-11-03 16:53:32', '2024-11-03 23:45:17'),
+(9, 'USUARIO ADMINISTRADOR 2', 'admin2@example2.com', '$2a$10$i5jWgOl3UOm61uQA2Q59qud6fXULWkIWr5meLlt6ITz3I9WUmLZNq', 'admin', NULL, '2024-11-03 22:37:20', '2024-11-03 22:37:20'),
+(10, 'Administrado3', 'admin3@ejemplo3.com', '$2a$10$/Bar4VJn.tiysD0rICFDf..GgHutXg0qi86Z4CwbEz/gVXj9Kcd4.', 'admin', NULL, '2024-11-03 23:59:04', '2024-11-03 23:59:04'),
+(11, 'Administrador4', 'admin3@example3.com', '$2b$10$1q3v7aEi.rqdQ1Y.ajOfD.1SSrvl2uc0za8a3tsTk5InDxvRKgYj6', 'admin', '1730679248655.jpg', '2024-11-04 00:14:08', '2024-11-04 00:14:08'),
+(21, 'Administrador6', 'juann@hotmail.com', '$2b$10$M0HflZNleo9NoY93N6RUFOCdxRoiZUu7Q56/wyQMChgl9VK/3cuEC', 'admin', '1730679575325.jpg', '2024-11-04 00:19:35', '2024-11-04 00:19:35'),
+(22, 'Administrador7', 'juannn@hotmail.com', '$2b$10$1XXXm1tVWQ4uskNfHYI58.nplU7WWD8CLWYkDzkVL2zqK72c0nETW', 'admin', '1730679588908.jpg', '2024-11-04 00:19:48', '2024-11-05 03:18:06'),
+(23, 'Administrador8 ', 'juannnn@hotmail.com', '$2a$10$j6C3CpPkkqw7i3uoiQUvc.Ab2o7.GYt42x1Rrp.afxqRo3AMHwJiS', 'admin', NULL, '2024-11-05 03:08:56', '2024-11-05 03:08:56');
 
 --
 -- Índices para tablas volcadas
@@ -212,7 +220,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `mesas`
 --
 ALTER TABLE `mesas`
-  MODIFY `id_mesa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_mesa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `platos`
@@ -224,19 +232,19 @@ ALTER TABLE `platos`
 -- AUTO_INCREMENT de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `turnos`
 --
 ALTER TABLE `turnos`
-  MODIFY `id_turno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_turno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Restricciones para tablas volcadas
